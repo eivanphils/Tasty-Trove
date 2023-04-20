4b7a6d691be793ae7fe24500a71dbbcaa48431f0
Phils Garcia <eivanphils@gmail.com>
2023-04-20 13:49:19 -0400

    cambio de titulo y desactivar el modo debug

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Tasty Trove',
       theme: ThemeData(
         // This is the theme of your application.
         //
