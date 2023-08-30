1e7dc010b97573bf4322364725f75fe14498ef9b
Phils Garcia <eivanphils@gmail.com>
2023-08-30 17:40:40 -0400

    Agregado paquetes de firebase para el cloudMessaging y solucion de problemas con ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'package:tasty_trove/providers/providers.dart';
 import 'package:tasty_trove/router/app_routes.dart';
 import 'package:tasty_trove/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const AppState());
 }
 
