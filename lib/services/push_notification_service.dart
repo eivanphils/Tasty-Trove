816bb0b5fa779633033e2b6785f2cdb210106781
Phils Garcia <eivanphils@gmail.com>
2023-09-18 15:03:42 -0300

    Agregado metodos para recibir notificaciones

--- lib/services/push_notification_service.dart
+++ lib/services/push_notification_service.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+import 'package:tasty_trove/firebase_options.dart';
+
 // 14:76:AA:67:77:3B:9F:BD:DC:3F:60:E2:0B:9F:39:CD:37:AD:3D:A9
 
 // Platform  Firebase App Id
@@ -12,13 +14,39 @@ class PushNotificationService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
 
+  static Future _backgroundHandler(RemoteMessage message) async {
+    print('_backgroundHandler ${message.messageId}');
+  }
+
+  static Future _onMessageHandler(RemoteMessage message) async {
+    print('_onMessageHandler ${message.messageId}');
+  }
+
+  static Future _onMessageOpenAppHandler(RemoteMessage message) async {
+    print('_onMessageOpenAppHandler ${message.messageId}');
+  }
+
   static Future initializeApp() async {
     //Push notification
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+
+    await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+        alert: true, badge: true, sound: true);
+
     token = await FirebaseMessaging.instance.getToken();
     print('token $token');
 
-  
+    // Handlers
+    // When app is in background
+    FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+
+    //  When app is open
+    FirebaseMessaging.onMessage.listen(_onMessageHandler);
+
+    // When app is closed
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenAppHandler);
+
     //local Notification
   }
 }
